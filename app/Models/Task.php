01109676785997cce7ae48b0a8d242c9205d412d
Yuri <yuri.pires.silveira@gmail.com>
2026-02-01 14:23:13 -0300

    refactor: adicionando fillable nos models

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+        'user_id'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
