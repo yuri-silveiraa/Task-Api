b2a25450840d420d00b274b3c6a3a77d6c53aa25
Yuri <yuri.pires.silveira@gmail.com>
2026-02-02 10:05:15 -0300

    refactor: adicionando hasfactory ao model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use HasFactory,Notifiable;
 
     protected $fillable = [
         'name',
